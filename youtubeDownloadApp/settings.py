a39036b59f5432a2086d94d8b5484a3ac52cee01
chung sarit <sarithchung168@gmail.com>
2023-02-01 21:23:24 +0900

    hope

--- youtubeDownloadApp/settings.py
+++ youtubeDownloadApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jwu*aocp40!*0f%=fd0v@h8p4&zvk6t$y$04s$4f$_g3@*q37f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
